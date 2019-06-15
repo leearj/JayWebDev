6db292f14b2977d564f547ed6c371c9fdd63cab9
JaeyLee <jaeseungl_github@hotmail.com>
2019-06-15 05:09:31 -0700

    prints "The temperature today is 68 degress Farenheit."

--- JavaScript/Temperature/temperature.js
+++ JavaScript/Temperature/temperature.js
@@ -1,6 +1,8 @@
 
-// This app.js will convertKelvin to Celsius, then to Farenheit.
+// This will convertKelvin to Celsius, then to Farenheit.
 
-const todayTempKelvin = 293;
-console.log(todayTempKelvin);
-console.log(`Today weather: ${todayTempKelvin}`);
+const kelvin = 293; // Today's temperature in kelvin.
+const celcius = kelvin - 273; // celcius is 273 less than kelvin.
+const farenheit = Math.floor(celcius * (9/5) + 32); // farenheit formula, and round down.
+
+console.log(`The temperature today is ${farenheit} degress Farenheit.`);
